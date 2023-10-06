62eb06859dd7a606f25b74db10c9839f78fce286
Victor Paulo Oliveira De Souza <vitorpaulo.pvc22@outlook.com>
2023-10-05 21:44:30 -0300

    curso

---
+++ aula17.py
@@ -0,0 +1,25 @@
+# if / elif     / else
+# se / se não se / se não
+Condicao1 = False
+Condicao2 = False
+Condicao3 = True
+Condicao4 = False
+
+
+if Condicao1:
+    print('Código para condição 1')
+elif Condicao2:
+    print('Código para condição 2')     
+elif Condicao3:
+    print('Código para condição 3')     
+elif Condicao4:
+    print('Código para condição 4')    
+else:    
+    print('Nenhuma condção foi satisfeita.')  
+
+if 10 == 10:   
+    print('Outro if')
+
+print('Fora do if')
+
+
